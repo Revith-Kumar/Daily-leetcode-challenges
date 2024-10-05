dfdc790b0d2b2b91221834aba1e3f03ec91a6b99
Revith kumar <157215286+Revith-Kumar@users.noreply.github.com>
2024-10-05 10:02:46 +0530

    Time: 28 ms (37.38%), Space: 56.4 MB (59.16%) - LeetHub

--- 1694-make-sum-divisible-by-p/1694-make-sum-divisible-by-p.java
+++ 1694-make-sum-divisible-by-p/1694-make-sum-divisible-by-p.java
@@ -2,32 +2,25 @@ import java.util.HashMap;
 
 class Solution {
     public int minSubarray(int[] nums, int p) {
-        long totalSum = 0;
-        for (int num : nums) {
-            totalSum += num;
+        long total = 0;
+        for(int i=0;i<nums.length;i++){
+            total+=nums[i];
         }
-
-        // Find remainder when total sum is divided by p
-        int rem = (int)(totalSum % p);
-        if (rem == 0) return 0; // If remainder is 0, no subarray needs to be removed
-
-        HashMap<Integer, Integer> prefixMod = new HashMap<>();
-        prefixMod.put(0, -1);  // Initialize to handle full prefix
-        long prefixSum = 0;
-        int minLength = nums.length;
-
-        for (int i = 0; i < nums.length; ++i) {
-            prefixSum += nums[i];
-            int currentMod = (int)(prefixSum % p);
-            int targetMod = (currentMod - rem + p) % p;
-
-            if (prefixMod.containsKey(targetMod)) {
-                minLength = Math.min(minLength, i - prefixMod.get(targetMod));
-            }
-
-            prefixMod.put(currentMod, i);
+        int rem = (int)(total%p);
+        if(rem==0) return 0;
+        HashMap<Integer,Integer> rev = new HashMap<>();
+        rev.put(0,-1);
+        long psum = 0;
+        int mlength=nums.length;
+        for(int i=0;i<nums.length;i++){
+           psum= psum+nums[i];
+           int current = (int)(psum%p);
+           int target = (current-rem+p)%p;
+           if(rev.containsKey(target)){
+            mlength = Math.min(mlength,i-rev.get(target));
+           } 
+           rev.put(current,i);
         }
-
-        return minLength == nums.length ? -1 : minLength;
+        return mlength==nums.length?-1:mlength;
     }
 }
