62368af32873987a82a5c1bdb4b27d552f0ba9fa
Revith kumar <157215286+Revith-Kumar@users.noreply.github.com>
2024-10-21 10:03:33 +0530

    Time: 50 ms (25.49%), Space: 45 MB (46.08%) - LeetHub

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.java
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.java
@@ -2,33 +2,22 @@ import java.util.HashSet;
 
 class Solution {
     public int maxUniqueSplit(String s) {
-        // Use HashSet instead of ArrayList for faster lookup of unique substrings
-        return split(s, new HashSet<>(), 0);
+        return backtrack(s, new HashSet<>(), 0);
     }
 
-    private int split(String s, HashSet<String> used, int start) {
-        // If we've reached the end of the string, no further split is possible
-        if (start == s.length()) {
-            return 0;
-        }
-
-        int maxCount = 0;
-        String currentSubstr = "";
-
-        // Explore substrings starting from 'start'
-        for (int i = start; i < s.length(); i++) {
-            currentSubstr += s.charAt(i);
-
-            // If this substring hasn't been used before
-            if (!used.contains(currentSubstr)) {
-                used.add(currentSubstr);  // Mark substring as used
-                // Recursively split the rest of the string and count unique splits
-                int count = 1 + split(s, used, i + 1);
-                maxCount = Math.max(maxCount, count);  // Update max count of unique splits
-                used.remove(currentSubstr);  // Backtrack by removing substring from set
+    private int backtrack(String s, HashSet<String> rev, int start) {
+        if(start==s.length()) return 0;
+        int maxcount =0;
+        String str = "";
+        for(int i=start;i<s.length();i++){
+            str+= s.charAt(i);
+            if(!rev.contains(str)){
+                rev.add(str);
+                int count = 1+backtrack(s,rev,i+1);
+                maxcount=Math.max(count,maxcount);
+                rev.remove(str);
             }
-        }
-
-        return maxCount;
-    }
+       }
+         return maxcount;
+}
 }
